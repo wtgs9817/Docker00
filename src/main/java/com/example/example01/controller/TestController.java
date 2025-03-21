3ad43a6c46cce32a30d2172c56303e4b44c536cb
KMC\김명찬 <wtgs9817@naver.com>
2025-03-22 02:18:53 +0900

    git 연습중

--- src/main/java/com/example/example01/controller/TestController.java
+++ src/main/java/com/example/example01/controller/TestController.java
@@ -3,13 +3,16 @@ package com.example.example01.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
 
     @GetMapping("/test")
-    public String test() {
+    public String test(Model model) {
+
+        model.addAttribute("message", "Spring Boot 테스트");
         System.out.println("테스트중");
         return "test01";
     }
